1b561d809053fb62a55c23d2893b89ecb48cff51
beniciovargas <beniciorvargas@gmail.com>
2018-12-17 06:52:44 -0800

    added notes to deedbar

--- client/src/components/DeedBar/DeedBar.js
+++ client/src/components/DeedBar/DeedBar.js
@@ -5,10 +5,13 @@ class DeedBar extends Component {
     state={
         deedPosts:[],
     }
+
+// trying to figure out how to get data from mongo to load here
+//changed .json to .text so page wouldnt break but needs to be changed
     componentDidMount() {
         console.log('doing thing');
         fetch('/api/mongodb/santas_test/')
-          .then(response => response.text())
+          .then(response => response.json())
           .then(data => {
             console.log('Got data back', data);
             this.setState({
